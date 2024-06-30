90d72673f75120d64c2c52f844fb6d407ec6c845
APSK22 <158535097+APSK22@users.noreply.github.com>
2024-06-30 14:25:32 +0530

    Added New UI

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Board from './components/Board';
+
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-br from-slate-900 to-slate-950">
+      <Board />
+    </div>
+  );
+}
